a71b519bf5c12403e847a59952f1343b5be973c2
James <james_hadoop@163.com>
2017-05-07 02:12:51 +0800

    add JamesConfig

--- src/main/webapp/org/cboard/ng-config.js
+++ src/main/webapp/org/cboard/ng-config.js
@@ -2,160 +2,155 @@
  * Created by Peter on 2016/10/22.
  */
 
-angular.module('cBoard').config([ '$stateProvider', function($stateProvider) {
-	$stateProvider.state('dashboard', {
-		url : '/dashboard',
-		abstract : true,
-		template : '<div ui-view></div>'
-	}).state('mine', {
-		url : '/mine',
-		abstract : true,
-		template : '<div ui-view></div>'
-	}).state('mine.view', {
-		url : '/{id}',
-		params : {
-			id : null
-		},
-		templateUrl : 'org/cboard/view/dashboard/view.html',
-		controller : 'dashboardViewCtrl'
-	}).state('dashboard.category', {
-		url : '/{category}',
-		params : {
-			category : null
-		},
-		abstract : true,
-		template : '<div ui-view></div>',
-	}).state('dashboard.category.view', {
-		url : '/{id}',
-		params : {
-			id : null
-		},
-		templateUrl : 'org/cboard/view/dashboard/view.html',
-		controller : 'dashboardViewCtrl'
-	}).state('config', {
-		url : '/config',
-		abstract : true,
-		template : '<div ui-view></div>'
-	}).state('config.board', {
-		url : '/board',
-		templateUrl : 'org/cboard/view/config/board.html',
-		controller : 'boardCtrl'
-	}).state('config.widget', {
-		url : '/widget',
-		params : {
-			id : null
-		},
-		templateUrl : 'org/cboard/view/config/widget.html',
-		controller : 'widgetCtrl'
-	}).state('config.datasource', {
-		url : '/datasource',
-		templateUrl : 'org/cboard/view/config/datasource.html',
-		controller : 'datasourceCtrl'
-	}).state('config.JamesConfig', {
-		url : '/datasource',
-		templateUrl : 'org/cboard/view/config/JamesConfig.html',
-		controller : 'JamesConfigCtrl'
-	}).state('config.category', {
-		url : '/category',
-		templateUrl : 'org/cboard/view/config/category.html',
-		controller : 'categoryCtrl'
-	}).state('config.dataset', {
-		url : '/dataset',
-		templateUrl : 'org/cboard/view/config/dataset.html',
-		controller : 'datasetCtrl'
-	}).state('admin', {
-		url : '/admin',
-		abstract : true,
-		template : '<div ui-view></div>'
-	}).state('admin.user', {
-		url : '/user',
-		templateUrl : 'org/cboard/view/admin/user.html',
-		controller : 'userAdminCtrl'
-	});
 
-} ]);
+angular.module('cBoard').config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+        .state('dashboard', {
+            url: '/dashboard',
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        .state('mine', {
+            url: '/mine',
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        .state('mine.view', {
+            url: '/{id}',
+            params: {id: null},
+            templateUrl: 'org/cboard/view/dashboard/view.html',
+            controller: 'dashboardViewCtrl'
+        })
+        .state('dashboard.category', {
+            url: '/{category}',
+            params: {category: null},
+            abstract: true,
+            template: '<div ui-view></div>',
+        })
+        .state('dashboard.category.view', {
+            url: '/{id}',
+            params: {id: null},
+            templateUrl: 'org/cboard/view/dashboard/view.html',
+            controller: 'dashboardViewCtrl'
+        })
+        .state('config', {
+            url: '/config',
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        .state('config.board', {
+            url: '/board',
+            templateUrl: 'org/cboard/view/config/board.html',
+            controller: 'boardCtrl'
+        })
+        .state('config.widget', {
+            url: '/widget',
+            params: {id: null},
+            templateUrl: 'org/cboard/view/config/widget.html',
+            controller: 'widgetCtrl'
+        })
+        .state('config.datasource', {
+            url: '/datasource',
+            templateUrl: 'org/cboard/view/config/datasource.html',
+            controller: 'datasourceCtrl'
+        })
+        .state('config.JamesConfig', {
+            url: '/JamesConfig',
+            templateUrl: 'org/cboard/view/config/JamesConfig.html',
+            controller: 'JamesConfigCtrl'
+        })
+        .state('config.category', {
+            url: '/category',
+            templateUrl: 'org/cboard/view/config/category.html',
+            controller: 'categoryCtrl'
+        }).state('config.dataset', {
+            url: '/dataset',
+            templateUrl: 'org/cboard/view/config/dataset.html',
+            controller: 'datasetCtrl'
+        }).state('admin', {
+            url: '/admin',
+            abstract: true,
+            template: '<div ui-view></div>'
+        }).state('admin.user', {
+            url: '/user',
+            templateUrl: 'org/cboard/view/admin/user.html',
+            controller: 'userAdminCtrl'
+        });
 
-angular.module('cBoard').factory('sessionHelper',
-		[ "$rootScope", function($rootScope) {
-			var sessionHelper = {
-				responseError : function(response) {
-					if (response.status == -1) {
-						window.location.href = "/";
-					} else {
-						return response;
-					}
-				}
-			};
-			return sessionHelper;
-		} ]);
+}]);
 
-angular
-		.module('cBoard')
-		.config(
-				function($httpProvider) {
-					$httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded';
-					$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+angular.module('cBoard').factory('sessionHelper', ["$rootScope", function ($rootScope) {
+    var sessionHelper = {
+        responseError: function (response) {
+            if (response.status == -1) {
+                window.location.href = "/";
+            } else {
+                return response;
+            }
+        }
+    };
+    return sessionHelper;
+}]);
 
-					// Override $http service's default transformRequest
-					$httpProvider.defaults.transformRequest = [ function(data) {
-						/**
-						 * The workhorse; converts an object to
-						 * x-www-form-urlencoded serialization.
-						 * 
-						 * @param {Object}
-						 *            obj
-						 * @return {String}
-						 */
-						var param = function(obj) {
-							var query = '';
-							var name, value, fullSubName, subName, subValue, innerObj, i;
 
-							for (name in obj) {
-								value = obj[name];
+angular.module('cBoard').config(function ($httpProvider) {
+    $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded';
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-								if (value instanceof Array) {
-									for (i = 0; i < value.length; ++i) {
-										subValue = value[i];
-										fullSubName = name + '[' + i + ']';
-										innerObj = {};
-										innerObj[fullSubName] = subValue;
-										query += param(innerObj) + '&';
-									}
-								} else if (value instanceof Object) {
-									for (subName in value) {
-										subValue = value[subName];
-										fullSubName = name + '[' + subName
-												+ ']';
-										innerObj = {};
-										innerObj[fullSubName] = subValue;
-										query += param(innerObj) + '&';
-									}
-								} else if (value !== undefined
-										&& value !== null) {
-									query += encodeURIComponent(name) + '='
-											+ encodeURIComponent(value) + '&';
-								}
-							}
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function (data) {
+        /**
+         * The workhorse; converts an object to x-www-form-urlencoded serialization.
+         * @param {Object} obj
+         * @return {String}
+         */
+        var param = function (obj) {
+            var query = '';
+            var name, value, fullSubName, subName, subValue, innerObj, i;
 
-							return query.length ? query.substr(0,
-									query.length - 1) : query;
-						};
+            for (name in obj) {
+                value = obj[name];
 
-						return angular.isObject(data)
-								&& String(data) !== '[object File]' ? param(data)
-								: data;
-					} ];
+                if (value instanceof Array) {
+                    for (i = 0; i < value.length; ++i) {
+                        subValue = value[i];
+                        fullSubName = name + '[' + i + ']';
+                        innerObj = {};
+                        innerObj[fullSubName] = subValue;
+                        query += param(innerObj) + '&';
+                    }
+                } else if (value instanceof Object) {
+                    for (subName in value) {
+                        subValue = value[subName];
+                        fullSubName = name + '[' + subName + ']';
+                        innerObj = {};
+                        innerObj[fullSubName] = subValue;
+                        query += param(innerObj) + '&';
+                    }
+                } else if (value !== undefined && value !== null) {
+                    query += encodeURIComponent(name) + '='
+                        + encodeURIComponent(value) + '&';
+                }
+            }
 
-					$httpProvider.interceptors.push('sessionHelper');
+            return query.length ? query.substr(0, query.length - 1) : query;
+        };
 
-				});
+        return angular.isObject(data) && String(data) !== '[object File]'
+            ? param(data)
+            : data;
+    }];
 
-angular.module('cBoard').config(
-		function($translateProvider, $translatePartialLoaderProvider) {
-			$translatePartialLoaderProvider.addPart('cboard');
-			$translateProvider.useLoader('$translatePartialLoader', {
-				urlTemplate : 'i18n/{lang}/{part}.json'
-			});
+    $httpProvider.interceptors.push('sessionHelper');
 
-			$translateProvider.preferredLanguage(settings.preferredLanguage);
-		});
+});
+
+
+angular.module('cBoard').config(function ($translateProvider, $translatePartialLoaderProvider) {
+    $translatePartialLoaderProvider.addPart('cboard');
+    $translateProvider.useLoader('$translatePartialLoader', {
+        urlTemplate: 'i18n/{lang}/{part}.json'
+    });
+
+    $translateProvider.preferredLanguage(settings.preferredLanguage);
+});
